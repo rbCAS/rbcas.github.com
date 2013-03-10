d1613f71ecdac49c22a1ff176c70cec523bd1405
Samuel Sieg <samuel.sieg@me.com>
2013-03-10 20:59:37 +0100

    added screenshots (needs improvement)

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,3 +1,4 @@
+// doc toc
 $('.toc li:first-child').click(function(e) {
   e.stopPropagation();
   $('.toc li:not(:first-child)').toggle();
@@ -6,3 +7,9 @@ $('.toc li:first-child').click(function(e) {
 $(document).click(function() {
   $('.toc li:not(:first-child)').hide();
 });
+
+// screenshots
+$("section.screenshots a").fancybox({
+    openEffect  : 'none',
+    closeEffect : 'none'
+  });
